0f0ad990edba9754b6dad834410f7b053718ff05
Kotaro <c0a24099c5@edu.teu.ac.jp>
2026-04-21 14:57:37 +0900

    練習３、４

--- ex2/dodge_bomb.py
+++ ex2/dodge_bomb.py
@@ -7,6 +7,22 @@ import random
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.Rect):
+    """
+    Rectが画面内かどうかを判定
+    戻り値：(横方向, 縦方向) のタプル
+    True：画面内 / False：画面外
+    """
+    yoko = True
+    tate = True
+
+    if rct.left < 0 or rct.right > WIDTH:
+        yoko = False
+    if rct.top < 0 or rct.bottom > HEIGHT:
+        tate = False
+
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -38,39 +54,49 @@ def main():
     while True:
         screen.blit(bg_img, [0, 0]) 
 
-        # 移動
-        bb_rct.move_ip(vx, vy)
-
-        # 描画
-        screen.blit(bb_img, bb_rct)
-
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        
 
+        # キー入力
         key_lst = pg.key.get_pressed()
-        
 
-        DELTA = { #辞書
-            pg.K_UP:   (0, -5), #上
-            pg.K_DOWN: (0,  5), #下
-            pg.K_LEFT: (-5, 0), #左
-            pg.K_RIGHT:(5,  0), #右
+        DELTA = {
+            pg.K_UP:   (0, -5),
+            pg.K_DOWN: (0,  5),
+            pg.K_LEFT: (-5, 0),
+            pg.K_RIGHT:(5,  0),
         }
 
         sum_mv = [0, 0]
-
         for key, mv in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
 
-        
+        # 爆弾
+        bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+        screen.blit(bb_img, bb_rct)
+
+        # こうかとん
+        old_rct = kk_rct.copy()
         kk_rct.move_ip(sum_mv)
+        yoko, tate = check_bound(kk_rct)
+        if not yoko or not tate:
+            kk_rct = old_rct
         screen.blit(kk_img, kk_rct)
+
+        # 衝突
+        if kk_rct.colliderect(bb_rct):
+            print("Game Over")
+            return
+
         pg.display.update()
-        tmr += 1
         clock.tick(50)
 
 
